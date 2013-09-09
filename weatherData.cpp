3fb7088bb4da131ded2bc0388ebeadfcda312aa3
vishal salgotra <salgotra.vishal@gmail.com>
2013-09-09 08:11:37 -0700

    added getter setters for weatherData

--- weatherData.cpp
+++ weatherData.cpp
@@ -9,10 +9,37 @@ class Private {
 		Private() { 
 			temp = humidity = pressure = 0.0;
 		}
+
+		void setTemperature(float);
+		void setHumidity(float);
+		void setPressure(float);
+		
 };
 
 weatherData::weatherData(): d(new Private) {
 
 }
 
+float weatherData::getTemperature() {
+	return d->temp;
+}
 
+float weatherData::getHumidity() {
+	return d->humidity;
+}
+
+float weatherData::getPressure() {
+	return d->pressure;
+}
+
+void Private::setTemperature(float temp) {
+	d->temp = temp;
+}
+
+void Private::setHumidity(float humidity) {
+	d->humidity = humidity;
+}
+
+void Private::setPressure(float pressure) {
+	d->pressure = pressure;
+}
